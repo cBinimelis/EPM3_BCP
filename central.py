10ed229ae30ffaa684cff0f41bf6e67f9e840df2
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-09-09 19:03:29 -0300

    Ejercicio completado
    Se agregan al repositorio los archivos con el ejercicio completo

---
+++ central.py
@@ -0,0 +1,220 @@
+# Parte 1: Cargar los datos
+def cargar_datos(lineas_archivo):
+    peliculas = list(lineas_archivo)
+    filtro_generos = set()
+    generos_peliculas = list()
+    peliculas_genero = list()
+    info_peliculas = list()
+
+    for pelicula in peliculas:
+        info = pelicula.split(",")
+        nombre = info[0]
+        popularidad = info[1]
+        puntaje = info[2]
+        votos = info[3]
+        generos = info[4].split(";")
+        for genero in generos:
+            filtro_generos.add(genero)
+        info_peliculas.append((nombre, popularidad, puntaje, votos, generos))
+
+    # Cargar generos a la lista
+    for genero in filtro_generos:
+        generos_peliculas.append(genero)
+
+    for genero in filtro_generos:
+        lista_peliculas = list()
+        for pelicula in info_peliculas:
+            if genero in pelicula[4]:
+                lista_peliculas.append(pelicula[0])
+        peliculas_genero.append((genero, lista_peliculas))
+
+    return generos_peliculas, peliculas_genero, info_peliculas
+
+
+# Parte 2: Completar las consultas
+def obtener_puntaje_y_votos(nombre_pelicula):
+    # Cargar las lineas con la data del archivo
+    lineas_archivo = leer_archivo()
+    puntaje_y_votos = None
+    peliculas = list(lineas_archivo)
+    for pelicula in peliculas:
+        info = pelicula.split(",")
+        if nombre_pelicula in info[0]:
+            puntaje = info[2]
+            votos = info[3]
+            puntaje_y_votos = puntaje, votos
+    return puntaje_y_votos
+
+
+def filtrar_y_ordenar(genero_pelicula):
+    # Cargar las lineas con la data del archivo
+    lineas_archivo = leer_archivo()
+    peliculas = list(lineas_archivo)
+    listado_peliculas = list()
+
+    for pelicula in peliculas:
+        info = pelicula.split(",")
+        generos = info[4].split(";")
+        for genero in generos:
+            if genero_pelicula in genero:
+                listado_peliculas.append(info[0])
+                break
+
+    listado_peliculas.sort(reverse=True)
+    return listado_peliculas
+
+
+def obtener_estadisticas(genero_pelicula, criterio):
+    # Cargar las lineas con la data del archivo
+    lineas_archivo = leer_archivo()
+    peliculas = list(lineas_archivo)
+    maximo = 0
+    minimo = 9999999
+    promedio = 0
+    suma = 0
+    opcion = 0
+    total_peliculas = 0
+    redondeo = 0
+    if criterio == "popularidad":
+        opcion = 1
+        redondeo = 3
+    elif criterio == "voto promedio":
+        opcion = 2
+        redondeo = 1
+    else:
+        opcion = 3
+
+    for pelicula in peliculas:
+        info = pelicula.split(",")
+        generos = info[4].split(";")
+        for genero in generos:
+            if genero_pelicula in genero:
+                total_peliculas += 1
+                valor = float(info[opcion])
+                suma += valor
+                if valor > maximo:
+                    maximo = valor
+                if valor < minimo:
+                    minimo = valor
+
+    promedio = suma / total_peliculas
+    return [maximo, minimo, round(promedio, redondeo)]
+
+
+# NO ES NECESARIO MODIFICAR DESDE AQUI HACIA ABAJO
+
+
+def solicitar_accion():
+    print("\n¿Qué desea hacer?\n")
+    print("[0] Revisar estructuras de datos")
+    print("[1] Obtener puntaje y votos de una película")
+    print("[2] Filtrar y ordenar películas")
+    print("[3] Obtener estadísticas de películas")
+    print("[4] Salir")
+
+    eleccion = input("\nIndique su elección (0, 1, 2, 3, 4): ")
+    while eleccion not in "01234":
+        eleccion = input("\nElección no válida.\nIndique su elección (0, 1, 2, 3, 4): ")
+    eleccion = int(eleccion)
+    return eleccion
+
+
+def leer_archivo():
+    lineas_peliculas = []
+    with open("movies.csv", "r", encoding="utf-8") as datos:
+        for linea in datos.readlines()[1:]:
+            lineas_peliculas.append(linea.strip())
+    return lineas_peliculas
+
+
+def revisar_estructuras(generos_peliculas, peliculas_por_genero, info_peliculas):
+    print("\nGéneros de películas:")
+    for genero in generos_peliculas:
+        print(f"    - {genero}")
+
+    print("\nTítulos de películas por genero:")
+    for genero in peliculas_por_genero:
+        print(f"    genero: {genero[0]}")
+        for titulo in genero[1]:
+            print(f"        - {titulo}")
+
+    print("\nInformación de cada película:")
+    for pelicula in info_peliculas:
+        print(f"    Nombre: {pelicula[0]}")
+        print(f"        - Popularidad: {pelicula[1]}")
+        print(f"        - Puntaje Promedio: {pelicula[2]}")
+        print(f"        - Votos: {pelicula[3]}")
+        print(f"        - Géneros: {pelicula[4]}")
+
+
+def solicitar_nombre():
+    nombre = input("\nIngrese el nombre de la película: ")
+    return nombre
+
+
+def solicitar_genero():
+    genero = input("\nIndique el género de película: ")
+    return genero
+
+
+def solicitar_genero_y_criterio():
+    genero = input("\nIndique el género de película: ")
+    criterio = input(
+        "\nIndique el criterio (popularidad, voto promedio, cantidad votos): "
+    )
+    return genero, criterio
+
+
+def main():
+    lineas_archivo = leer_archivo()
+    datos_cargados = True
+    try:
+        generos_peliculas, peliculas_por_genero, info_peliculas = cargar_datos(
+            lineas_archivo
+        )
+    except TypeError as error:
+        if "cannot unpack non-iterable NoneType object" in repr(error):
+            print(
+                "\nTodavía no puedes ejecutar el programa ya que no has cargado los datos\n"
+            )
+            datos_cargados = False
+    if datos_cargados:
+        salir = False
+        print("\n********** ¡Bienvenid@! **********")
+        while not salir:
+            accion = solicitar_accion()
+
+            if accion == 0:
+                revisar_estructuras(
+                    generos_peliculas, peliculas_por_genero, info_peliculas
+                )
+
+            elif accion == 1:
+                nombre_pelicula = solicitar_nombre()
+                ptje, votos = obtener_puntaje_y_votos(nombre_pelicula)
+                print(f"\nObteniendo puntaje promedio y votos de {nombre_pelicula}")
+                print(f"    - Puntaje promedio: {ptje}")
+                print(f"    - Votos: {votos}")
+
+            elif accion == 2:
+                genero = solicitar_genero()
+                nombres_peliculas = filtrar_y_ordenar(genero)
+                print(f"\nNombres de películas del género {genero} ordenados:")
+                for nombre in nombres_peliculas:
+                    print(f"    - {nombre}")
+
+            elif accion == 3:
+                genero, criterio = solicitar_genero_y_criterio()
+                estadisticas = obtener_estadisticas(genero, criterio)
+                print(f"\nEstadísticas de {criterio} de películas del género {genero}:")
+                print(f"    - Máximo: {estadisticas[0]}")
+                print(f"    - Mínimo: {estadisticas[1]}")
+                print(f"    - Promedio: {estadisticas[2]}")
+
+            else:
+                salir = True
+        print("\n********** ¡Adiós! **********\n")
+
+
+if __name__ == "__main__":
+    main()
